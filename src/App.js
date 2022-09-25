a5f050041ce6dd78c03414513adec31468d6f243
unknown <abdessamad.ghachi98@gmail.com>
2022-09-26 00:20:21 +0100

    refister- handleSubmit - watch - reset - formstate - defaultvalues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+import "./App.css";
+import React from "react";
+const data = [
+	{
+		firstname: "abdo",
+		lastname: "gh",
+		male: true,
+	},
+	{
+		firstname: "zozo",
+		lastname: "zozo",
+		male: false,
+	},
+	{
+		firstname: "toto",
+		lastname: "toto",
+		male: true,
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const {
+		register,
+		handleSubmit,
+		watch,
+		reset,
+		formState: { errors, isDirty, isValid },
+	} = useForm({
+		defaultValues: {
+			firstname: "",
+			lastname: "",
+			male: false,
+		},
+	});
+
+	const onSubmit = () => {
+		console.log(watch());
+	};
+
+	return (
+		<div>
+			hello
+			<form action="" onSubmit={handleSubmit(onSubmit)}>
+				<label htmlFor="">First Name :</label>
+				<input
+					type="text"
+					placeholder="first name"
+					{...register("firstname", {
+						required: "This champ is required",
+						minLength: { value: 5, message: "min length is 5" },
+						onBlur: () => console.log("I am out"),
+					})}
+				/>
+				<p>{errors && <span>this is required input</span>} </p>
+				<br />
+				<label htmlFor="">Last Name :</label>
+				<input type="text" placeholder="last name" {...register("lastname")} />
+				<br />
+				<label htmlFor="">Male :</label>
+				<input type="checkbox" {...register("male")} />
+				<br />
+				<input type="submit" value="Done!" disabled={!isValid} />
+				<button
+					disabled={!isDirty}
+					onClick={() =>
+						reset({
+							firstname: "I can overrid default value for one attr",
+						})
+					}
+				>
+					Reset!
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
